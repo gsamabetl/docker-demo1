2421f4ee0544827807f0aeaa5004e54a0ac4601d
gsamabetl <66405146+gsamabetl@users.noreply.github.com>
2021-09-29 21:31:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! Hello Siva! How are you!!!');
+  res.send('Hello World! Hello Siva! How are you!!! You are learning DevOps Course!!!');
 });
 
 var server = app.listen(3000, function () {
